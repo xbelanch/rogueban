23d3c79894228338397b72b36386bfc4c2ebf11e
Xavier Belanche <xbelanch@protonmail.com>
2022-08-02 09:03:53 +0200

    Implement simplistic ECS

--- rogueban.c
+++ rogueban.c
@@ -6,32 +6,29 @@
 #include "colors.h"
 #include "console.h"
 #include "sdl_helpers.h"
+#include "rogueban.h"
 
-#define TILESET "./Zilk_16x16.png"
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
-#define FONT_SIZE 16
-#define ZOOM 1
-#define NUM_ROWS SCREEN_HEIGHT / FONT_SIZE
-#define NUM_COLS SCREEN_WIDTH / FONT_SIZE
-#define FPS 60
-
-typedef struct {
-    int x, y;
-} Hero;
-
-Hero hero;
+GameObject *hero;
+GameObject *rock;
 
 void render_screen(SDL_Renderer *renderer, SDL_Texture *screen, Console *con)
 {
     console_clear(con);
-    console_putGlyphAt(con, '@', hero.x, hero.y, 0x00ff00ff, 0x0);
+
+    Position *rockPos = (Position *)getGameObjectComponent(rock, POSITION);
+    Outfit *rockOutfit = (Outfit *)getGameObjectComponent(rock, OUTFIT);
+    console_putGlyphAt(con, rockOutfit->glyph, rockPos->x, rockPos->y, rockOutfit->fgColor, rockOutfit->bgColor);
+
+    Position *heroPos = (Position *)getGameObjectComponent(hero, POSITION);
+    Outfit *heroOutfit = (Outfit *)getGameObjectComponent(hero, OUTFIT);
+    console_putGlyphAt(con, heroOutfit->glyph, heroPos->x, heroPos->y, heroOutfit->fgColor, heroOutfit->bgColor);
 
     SDL_UpdateTexture(screen, NULL, con->pixels, SCREEN_WIDTH * (sizeof(uint32_t)));
  	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, screen, NULL, NULL);
     SDL_RenderPresent(renderer);
 }
+
 int main(int argc, char *argv[])
 {
     (void) argc;
@@ -48,8 +45,19 @@ int main(int argc, char *argv[])
     console_setBitmapFont(con, TILESET, 0, FONT_SIZE, FONT_SIZE);
     SDL_Texture *screen = SCP(SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, SCREEN_WIDTH, SCREEN_HEIGHT));
 
-    hero = (Hero) { .x = NUM_COLS / 2, .y = NUM_ROWS / 2 };
+    // Add our hero to the game
+    hero = newGameObject();
+    Position heroPosition = { .id = hero->id, .x = (NUM_COLS / 2) - 1, .y = (NUM_ROWS / 2) - 1};
+    Outfit heroOutfit = { .id = hero->id, .glyph = '@', .fgColor = 0x00ff00ff, .bgColor = 0x0 };
+    addComponentToGameObject(hero, POSITION, &heroPosition);
+    addComponentToGameObject(hero, OUTFIT, &heroOutfit);
 
+    // Add a simple rock
+    rock = newGameObject();
+    Position rockPosition = { .id = rock->id, .x = rand() % NUM_COLS, .y = rand() % NUM_ROWS };
+    Outfit rockOutfit = { .id = rock->id, .glyph = 'A', .fgColor = 0xff0000ff, .bgColor = 0xffffffff };
+    addComponentToGameObject(rock, POSITION, &rockPosition);
+    addComponentToGameObject(rock, OUTFIT, &rockOutfit);
 
     // Game loop
     bool quit = false;
@@ -61,29 +69,32 @@ int main(int argc, char *argv[])
                 quit = true;
                 break;
             } else if (event.type == SDL_KEYDOWN) {
+
+                Position *heroPos = (Position *)getGameObjectComponent(hero, POSITION);
+
                 switch (event.key.keysym.sym) {
                 case SDLK_ESCAPE: {
                     quit = true;
                     break;
                 }
                 case SDLK_w: {
-                    if (hero.y > 0)
-                        hero.y--;
+                    if (heroPos->y > 0)
+                        heroPos->y--;
                     break;
                 }
                 case SDLK_s: {
-                    if (hero.y < NUM_ROWS - 1)
-                        hero.y++;
+                    if (heroPos->y < NUM_ROWS - 1)
+                        heroPos->y++;
                     break;
                 }
                 case SDLK_a: {
-                    if (hero.x > 0)
-                        hero.x--;
+                    if (heroPos->x > 0)
+                        heroPos->x--;
                     break;
                 }
                 case SDLK_d: {
-                    if (hero.x < NUM_COLS - 1)
-                        hero.x++;
+                    if (heroPos->x < NUM_COLS - 1)
+                        heroPos->x++;
                     break;
                 }
                 default: break;
@@ -92,8 +103,6 @@ int main(int argc, char *argv[])
         }
 
         render_screen(renderer, screen, con);
-
-
 
         const Uint32 duration = SDL_GetTicks() - start;
         const Uint32 delta_time_ms = 1000 / FPS;
---
+++ rogueban.h
@@ -0,0 +1,127 @@
+#ifndef _ROGUEBAN_H
+#define _ROGUEBAN_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+#include "console.h"
+
+#define TILESET "./Zilk_16x16.png"
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+#define FONT_SIZE 16
+#define ZOOM 1
+#define NUM_ROWS SCREEN_HEIGHT / FONT_SIZE
+#define NUM_COLS SCREEN_WIDTH / FONT_SIZE
+#define FPS 60
+#define MAX_GAME_OBJECTS_SIZE 1024
+
+typedef enum {
+    POSITION = 0,
+    OUTFIT,
+    PHYSICAL,
+    HEALTH,
+    MOVEMENT,
+
+    // Define other components above
+    MAX_SIZE_COMPONENTS
+} GameComponent;
+
+// Entities and Components
+// Based on https://en.wikipedia.org/wiki/Entity_component_system
+// Simplistic implementation in C: https://gamedev.stackexchange.com/questions/172584/how-could-i-implement-an-ecs-in-c
+
+typedef struct {
+    uint8_t x, y;
+} Hero;
+
+typedef struct {
+    uint32_t id;
+    void *components[MAX_SIZE_COMPONENTS];
+} GameObject;
+
+// Components
+typedef struct {
+    uint32_t id;
+    uint8_t x, y;
+} Position;
+
+typedef struct {
+    uint32_t id;
+    asciiChar glyph;
+    uint32_t fgColor;
+    uint32_t bgColor;
+} Outfit;
+
+typedef struct {
+    uint32_t id;
+    bool blocksMovement;
+    bool blocksSight;
+} Physical;
+
+GameObject gameObjects[MAX_GAME_OBJECTS_SIZE];
+Position positionGameObjects[MAX_GAME_OBJECTS_SIZE];
+Outfit outfitGameObjects[MAX_GAME_OBJECTS_SIZE];
+
+GameObject *newGameObject()
+{
+    GameObject *obj = NULL;
+    // First (amb naive) way for storing gameObjects
+    // @TODO: Try to rewrite this because trick of i = 1 when id is equal to 0 (02-08-2022)
+    for (size_t i = 1; i < MAX_GAME_OBJECTS_SIZE; ++i) {
+        if (gameObjects[i].id == 0) {
+            obj = &gameObjects[i];
+            obj->id = i;
+            break;
+        }
+    }
+    assert(obj != NULL);
+    return obj;
+}
+
+void addComponentToGameObject(GameObject *obj, GameComponent comp, void *compData)
+{
+    assert(obj->id != 0);
+    switch(comp) {
+    case POSITION: {
+        Position *position = &positionGameObjects[obj->id];
+        position->id = obj->id;
+        Position *data = (Position *) compData;
+        position->x = data->x;
+        position->y = data->y;
+        obj->components[comp] = &positionGameObjects[obj->id];
+        break;
+    }
+    case OUTFIT: {
+        Outfit *outfit = &outfitGameObjects[obj->id];
+        outfit->id = obj->id;
+        Outfit *data = (Outfit *) compData;
+        outfit->glyph = data->glyph;
+        outfit->fgColor = data->fgColor;
+        outfit->bgColor = data->bgColor;
+        obj->components[comp] = &outfitGameObjects[obj->id];
+        break;
+    }
+    default:
+        assert(false && "Unknow component");
+        exit(1);
+    }
+}
+
+void *getGameObjectComponent(GameObject *obj, GameComponent comp)
+{
+    return obj->components[comp];
+}
+
+void destroyGameObject(GameObject *obj)
+{
+    positionGameObjects[obj->id].id = 0;
+    outfitGameObjects[obj->id].id = 0;
+    // Set index object to zero
+    obj->id = 0;
+    // Clean up all components used by this object
+
+}
+
+
+#endif // _ROGUEBAN_H
